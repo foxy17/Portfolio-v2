66f057bc223c44c3a4a4488301a9b298e2384056
Arnav Chauhan <35425489+foxy17@users.noreply.github.com>
2025-04-06 15:09:42 +0530

    refactor(shared tailwind): :recycle: share tailwind config between both apps

--- packages/landing/data/api/base.ts
+++
@@ -1,3 +0,0 @@
-export default async function callServerSideAPI(url: RequestInfo) {
-    return await fetch(url).then((res) => res.json());
-}
--- packages/landing/lib/getElementHeightInView.ts
+++
@@ -1,6 +0,0 @@
-export const getElementHeightInView = (element: HTMLElement) => {
-    const heightInView = window.innerHeight - element.getBoundingClientRect().top;
-
-    // Prevent returning negatives
-    return heightInView > 0 ? heightInView : 0;
-};
